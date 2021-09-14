ac46c277d34467d4178330aa2ab5d26c1fdc8855
PushpPreet07 <82702275+PushpPreet07@users.noreply.github.com>
2021-09-14 19:35:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const inputfields=document.querySelectorAll("input");
+const btn=document.querySelectorAll("button");
+
+const userdata=[];
+
+document.querySelector(".cv-btn").onclick=function(){myFcn()}
+    function myFcn(){
+
+    let name=inputfields[0].value;
+    let email=inputfields[1].value;
+    let phone=inputfields[2].value;
+    userdata.push({name, email, phone});
+    alert("Button was clicked");
+    console.log(userdata);
+}
+// document.querySelector(".cv-btn").onClick=function(){myfcn()}
+// function myfcn(){
+//       alert("Your etails have been submitted successfully");
+// }
